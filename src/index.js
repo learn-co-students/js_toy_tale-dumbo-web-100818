ebcae80756580fee32925801bc68814bb0d593a4
github username <chao.alexander.123@gmail.com>
2018-11-25 00:04:58 -0500

    integrate like functionality with patch update to the db

--- src/index.js
+++ src/index.js
@@ -4,42 +4,10 @@ let nameInput = document.querySelector("#name");
 let imageInput = document.querySelector("#image");
 let addToy = false;
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-    toyForm.addEventListener("submit", postToy)
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
-
-
-function postToy() {
-  let toyData = {
-    name: nameInput.value,
-    image: imageInput.value,
-    likes: 0
-  }
-  
-  fetch("http://localhost:3000/toys", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify(toyData)
-  })
-  .then(resp => resp.json())
-  .then(json => console.log(json))
-};
-
 function fetchToys() {
   fetch("http://localhost:3000/toys")
-    .then(resp => resp.json())
-    .then(renderToys)
+  .then(resp => resp.json())
+  .then(renderToys)
 };
 
 function renderToys(json) {
@@ -63,8 +31,11 @@ function addCard(toy) {
   imgElement.src = toy.image;
   imgElement.classList.add("toy-avatar");
   likeElement.innerHTML = `${toy.likes} Likes`;
+  likeElement.id = toy.likes
   buttonElement.classList.add("like-btn");
   buttonElement.innerHTML = "Like <3"
+  buttonElement.onclick = addLike;
+  buttonElement.id = toy.id;
 
   cardElement.appendChild(nameElement);
   cardElement.appendChild(imgElement);
@@ -73,7 +44,63 @@ function addCard(toy) {
   return cardElement;
 };
 
+addBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block'
+    toyForm.addEventListener("submit", postToy)
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
+
+function postToy() {
+  event.preventDefault();
+
+  let toyData = {
+    name: nameInput.value,
+    image: imageInput.value,
+    likes: 0
+  }
+
+  fetch("http://localhost:3000/toys", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toyData)
+  })
+  .then(resp => resp.json())
+  .then(json => console.log("json"))
+};
+
+function addLike() {
+  let id = event.target.id
+  let likeElement = event.target.parentElement.children[2]
+  let updatedLikes = parseInt(likeElement.id) + 1
+  postLike(id, updatedLikes)
+  .then(function(json) {
+    likeElement.innerHTML = (`${updatedLikes} Likes`)
+    likeElement.id = updatedLikes
+  })
+}
+
+function postLike(id, updatedLikes) {
+  let likeData = {likes:  updatedLikes}
+
+  return fetch(`http://localhost:3000/toys/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(likeData)
+  })
+  .then(resp => resp.json())
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("Page has loaded!");
   fetchToys();
 });
