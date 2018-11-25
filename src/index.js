ad3d3e16e785328e0a3d997a1aeb5add666e503a
github username <chao.alexander.123@gmail.com>
2018-11-24 21:30:31 -0500

    fetch toys from localhost API to create and render cards to the DOM

--- src/index.js
+++ src/index.js
@@ -15,5 +15,45 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(renderToys)
+}
+
+function renderToys(json) {
+  const toyCollection = document.querySelector("#toy-collection");
+
+  for (toy of json) {
+    const card = addCard(toy);
+    toyCollection.appendChild(card);
+  };
+};
+
+function addCard(toy) {
+  const cardElement = document.createElement("div");
+  const nameElement = document.createElement("h2");
+  const imgElement = document.createElement("img");
+  const likeElement = document.createElement("p");
+  const buttonElement = document.createElement("button");
+
+  cardElement.classList.add("card");
+  nameElement.innerHTML = toy.name;
+  imgElement.src = toy.image;
+  imgElement.classList.add("toy-avatar");
+  likeElement.innerHTML = `${toy.likes} Likes`;
+  buttonElement.classList.add("like-btn");
+  buttonElement.innerHTML = "Like <3"
+
+  cardElement.appendChild(nameElement);
+  cardElement.appendChild(imgElement);
+  cardElement.appendChild(likeElement);
+  cardElement.appendChild(buttonElement);
+  return cardElement;
+}
 
 // OR HERE!
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("Page has loaded!");
+  fetchToys();
+});
